b972f162caef2c496590e0361b71b5ef542546d0
David-Koshkadze <rootUS3rr@pm.me>
2023-11-10 15:44:23 +0400

    messages and user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username: "John", password: "1234")
+User.create(username: "Jane", password: "1234")
