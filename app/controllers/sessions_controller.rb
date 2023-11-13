050f913d253f764e6d0c54a74ee4a658dc28239f
David-Koshkadze <rootUS3rr@pm.me>
2023-11-13 14:29:37 +0400

    user session and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,22 @@
-# frozen_string_literal: true
 class SessionsController < ApplicationController
   def new
   end
 
   def create
-    binding.break
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There are errors to login"
+      render "new", status: 422
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:info] = "You have logged out"
+    redirect_to login_path
   end
 end
