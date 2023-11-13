2eeb41f9513d418e3599bdfc4d38546aaf935ed0
David-Koshkadze <rootUS3rr@pm.me>
2023-11-13 12:37:46 +0400

    sign in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   root "chatroom#index"
-  get 'login', to: 'sessions#new'
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
 end
